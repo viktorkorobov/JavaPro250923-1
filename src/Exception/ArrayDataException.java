25732dcc89733094d4098017f0b4fe547ed561e8
viktorkorobov <viktorkorobov2012@gmail.com>
2023-11-20 17:53:44 +0200

    Homework11-1

---
+++ src/Exception/ArrayDataException.java
@@ -0,0 +1,7 @@
+package Exception;
+
+public class ArrayDataException extends Exception{
+    public ArrayDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/Exception/ArraySizeException.java
@@ -0,0 +1,7 @@
+package Exception;
+
+public class ArraySizeException extends Exception{
+    public ArraySizeException(String message) {
+        super(message);
+    }
+}
