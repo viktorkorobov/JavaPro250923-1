ade33b135df24fac255dfb27a7a47b8fe5836151
viktorkorobov <viktorkorobov2012@gmail.com>
2023-12-01 13:35:46 +0200

    Homework15-1

---
+++ src/ClientSocket.java
@@ -0,0 +1,36 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientSocket {
+    public static void main(String[] args) {
+      try {
+            Socket socket = new Socket("localhost", 8081);
+           Scanner scanner =new Scanner(System.in);
+            // Відправлення привітання серверу
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(scanner.next());
+
+            // Отримання відповіді від сервера
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String serverResponse = in.readLine();
+            System.out.println("Server: " + serverResponse);
+
+            if(serverResponse.equals("Що таке паляниця?")){
+                out.println(scanner.next());
+                String dateTime = in.readLine();
+                System.out.println("Server respone: " + dateTime);
+                serverResponse = in.readLine();
+                System.out.println("Server respone: " + serverResponse);
+            } else System.out.println("Incorrect answer");
+            // Закриття з'єднання
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8081);
+            System.out.println("Server is listening on port 8081");
+
+            // Очікування підключення клієнта
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected");
+
+            // Отримання введення від клієнта
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String clientGreeting = in.readLine();
+            System.out.println("Client: " + clientGreeting);
+
+            // Перевірка чи є російські букви в привітанні
+            if (containsRussianLetters(clientGreeting)) {
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                out.println("Що таке паляниця?");
+                String answer = in.readLine();
+                if (answer.equalsIgnoreCase("Хліб")) {
+                    out.println("Data and time: " + java.time.LocalDateTime.now());
+                    out.println("Bay!");
+                } else {
+                    out.println("Incorrect answer. Disconnect.");
+                    clientSocket.close();
+                }
+            } else {
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                out.println("Hello from the server!");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean containsRussianLetters(String input) {
+        // Перевірка чи є російські букви в рядку
+        return input.matches(".*[а-яА-Я].*");
+    }
+}
